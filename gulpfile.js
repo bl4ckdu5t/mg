bee78081bbb8b87d4424a6315288ac1de857d3c0
bl4ckdu5t <joerexko@gmail.com>
2015-10-14 05:08:51 -0500

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,89 @@
+var gulp = require('gulp'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    //imageResize = require('gulp-image-resize'),
+    //parallel = require('concurrent-transform'),
+    os = require('os'),
+    htmlmin = require('gulp-htmlmin'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    browserSync = require('browser-sync'),
+    reload = browserSync.reload;
+
+var postcss = require('gulp-postcss');
+var sass = require('gulp-sass');
+var concatCss = require('gulp-concat-css');
+var autoprefixer = require('autoprefixer');
+var mqpacker = require('css-mqpacker');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('css', function(){
+  var cssFiles = ['./src/scss/*.scss'];
+  return gulp.src(cssFiles)
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(concatCss('app.css'))
+    .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('postprocess', function(){
+  var processors = [
+    autoprefixer({browsers: ['last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4']}),
+    mqpacker
+  ];
+  return gulp.src('./dist/css/*.css')
+    .pipe(postcss(processors))
+    .pipe(gulp.dest('./dist/css'));
+});
+// Static server
+gulp.task('browser-sync', function(){
+  browserSync({
+    server: {
+      baseDir: "./"
+    }
+  });
+});
+
+// Concatenate & Minify
+gulp.task('scripts', function(){
+  var jsFiles = [
+    './src/components/jquery/dist/jquery.min.js',
+    './src/js/*.js'];
+  return gulp.src(jsFiles)
+    .pipe(sourcemaps.init())
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest('dist/js'))
+    .pipe(uglify())
+    .pipe(sourcemaps.write('../maps'))
+    .pipe(gulp.dest('dist/js'))
+    .pipe(reload({stream:true}));
+});
+
+// Images
+gulp.task('images', function(){
+  return gulp.src('./src/images/**/*')
+    /*.pipe(parallel(
+      imageResize({ width: 600 })
+    ))*/
+    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+    .pipe(gulp.dest('dist/images'))
+    .pipe(notify({ message: 'Images task complete' }));
+});
+
+// HTML
+gulp.task('html', function(){
+  return gulp.src('src/*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('./'))
+});
+
+// Watch
+gulp.task('watch', function(){
+  gulp.watch('./src/**/*.scss', ['css', browserSync.reload]);
+
+  gulp.watch('./src/**/*.js', ['scripts', browserSync.reload]);
+});
+
+
+gulp.task('default', ['css', 'postprocess','browser-sync', 'scripts', 'watch']);
---
+++ src/js/main.js
@@ -0,0 +1,99 @@
+function ready(cb) {
+	/in/.test(document.readyState) // in = loadINg
+		? setTimeout(ready.bind(null, cb), 9)
+		: cb();
+}
+ready(function(){
+	/*======= Waypoints http://imakewebthings.com/waypoints/ ============= */
+	var waypointRow1 = new Waypoint({
+	  element: document.querySelector('.waypoint'),
+	  handler: function() {
+	  	var one = document.getElementById('animation1'),
+	  	two = document.getElementById('animation2');
+	    one.classList.add('bounceInLeft');
+	    two.classList.add('bounceInRight');
+	    one.style.visibility = 'visible';
+	    two.style.visibility = 'visible';
+	  },
+	  offset: '70%'
+	});
+	var waypointRow2 = new Waypoint({
+		element: document.querySelector('.waypoint2'),
+		handler: function(){
+			var one = document.getElementById('animation3'),
+			two = document.getElementById('portfolio'),
+			three = document.getElementById('animation4');
+	    one.classList.add('zoomIn');
+	    two.classList.add('rubberBand');
+	    three.classList.add('tada');
+	    one.style.visibility = 'visible';
+	    two.style.visibility = 'visible';
+	    three.style.visibility = 'visible';
+		},
+		offset: '70%'
+	});
+	var waypointRow3 = new Waypoint({
+		element: document.querySelector('.waypoint3'),
+		handler: function(){
+			var one = document.getElementById('contact'),
+			two = document.getElementById('animation5'),
+			three = document.getElementById('animation6'),
+			four = document.getElementById('animation7');
+			one.classList.add('lightSpeedIn');
+			two.classList.add('rollIn');
+			three.classList.add('flipInY');
+			four.classList.add('fadeInUp');
+			one.style.visibility = 'visible';
+	    two.style.visibility = 'visible';
+	    three.style.visibility = 'visible';
+	    four.style.visibility = 'visible';
+		},
+		offset: '100%'
+	});
+	/*====== Lory Slider http://meandmax.github.io/lory/ ======*/
+	var simple = document.querySelector('.js_simple');
+  lory(simple, {
+      infinite: 1
+  });
+});
+/* Scroll to Hashbanged links */
+(function(){
+var speed = 500;
+var moving_frequency = 15; // Affects performance !
+var links = document.getElementsByTagName('a');
+var href;
+for(var i=0; i<links.length; i++){
+	href = (links[i].getAttributeNode('href') === undefined) ? null : links[i].getAttribute('href');
+	if(href !== null && href.length > 1 && href.substr(0, 1) == '#'){
+	  links[i].onclick = function(){
+	    var element;
+	    var href = this.getAttribute('href');
+	    if(element = document.getElementById(href.substr(1))){
+        var hop_count = speed/moving_frequency
+        var getScrollTopDocumentAtBegin = getScrollTopDocument();
+        var gap = (getScrollTopElement(element) - getScrollTopDocumentAtBegin) / hop_count;
+        for(var i = 1; i <= hop_count; i++){
+          (function(){
+	          var hop_top_position = gap*i;
+	          setTimeout(function(){  window.scrollTo(0, hop_top_position + getScrollTopDocumentAtBegin); }, moving_frequency*i);
+          })();
+        }
+	    }
+	    return false;
+    };
+	}
+}
+
+var getScrollTopElement =  function (e){
+  var top = 0;
+  while (e.offsetParent != undefined && e.offsetParent != null){
+    top += e.offsetTop + (e.clientTop != null ? e.clientTop : 0);
+    e = e.offsetParent;
+  }
+  return top;
+};
+
+var getScrollTopDocument = function(){
+  return document.documentElement.scrollTop + document.body.scrollTop;
+};
+})();
